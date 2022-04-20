19be16ad694acd2069faa2d4413cf025523d1be2
noamteyssier <22600644+noamteyssier@users.noreply.github.com>
2022-04-20 13:48:31 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='pypage',
+   version='0.0.1',
+   description='Python implementation of the PAGE algorithm',
+   author='Noam Teyssier',
+   packages=['pypage'],
+   install_requires=['numpy']
+)
