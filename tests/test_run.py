230c75d1f1900393288a8b3dfebe9d39413e1e38
Noam Teyssier <22600644+noamteyssier@users.noreply.github.com>
2022-04-22 11:20:20 -0700

    added testing + example data to test with

---
+++ tests/test_run.py
@@ -0,0 +1,30 @@
+"""Testing an example usage of the PAGE algorithm
+"""
+
+import pytest 
+import pandas as pd
+from pypage.page import PAGE
+from pypage.io import (
+        ExpressionProfile, 
+        GeneOntology)
+
+@pytest.fixture()
+def load_expression():
+    frame = pd.read_csv(
+        "example_data/AP2S1.tab.gz", 
+        sep="\t",
+        header=None,
+        names=["gene", "bin"])
+    return ExpressionProfile(frame)
+
+@pytest.fixture()
+def load_ontology():
+    return GeneOntology("example_data/GO_BP_2021_index.txt.gz")
+
+
+def test_run(load_expression, load_ontology):
+    p = PAGE(n_shuffle=5)
+    results = p.run(
+            load_expression,
+            load_ontology)
+
