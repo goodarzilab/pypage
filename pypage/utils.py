b7e9f48a1c0473b74a849db72d2bd8e6671545c9
Noam Teyssier <22600644+noamteyssier@users.noreply.github.com>
2022-04-22 10:26:49 -0700

    hypergeometric testing

--- pypage/utils.py
+++ pypage/utils.py
@@ -3,6 +3,8 @@
 
 import numpy as np
 import numba as nb
+from scipy.stats import hypergeom
+
 
 @nb.jit(nopython=True)
 def contingency_table(
@@ -40,3 +42,31 @@ def empirical_pvalue(
         return 0.
     else:
         return np.mean(array > value)
+
+
+def hypergeometric_test(
+        e_bool: np.ndarray, 
+        o_bool: np.ndarray) -> np.ndarray:
+    """
+    translating from: https://ars.els-cdn.com/content/image/1-s2.0-S1097276509008570-mmc1.pdf
+    
+    m = n: number of genes in the pathway
+    x = k: number of genes in the bin which are also in the pathway
+    n = N: number of genes in the bin
+    N = M: total number of genes
+    """
+    n = o_bool.sum()
+    k = (e_bool & o_bool).sum(axis=1)
+    N = e_bool.sum(axis=1)
+    M = o_bool.size
+
+    # parameterize hypergeometric distribution
+    hg = hypergeom(M, n, N)
+    
+    # measure overrepresentation
+    sf = hg.sf(k)
+    
+    # measure underrepresentation
+    cdf = hg.cdf(k)
+    
+    return np.stack([sf, cdf])
