b8b7634a9b5032bd9c5bdcfe366944ae067cba73
Noam Teyssier <22600644+noamteyssier@users.noreply.github.com>
2022-04-25 13:08:43 -0700

    restructure utils to work with histogram format and e_bins

--- pypage/utils.py
+++ pypage/utils.py
@@ -5,6 +5,96 @@ import numpy as np
 import numba as nb
 from scipy.stats import hypergeom
 
+@nb.jit(
+    nopython=True, 
+    nogil=True, 
+    cache=True, 
+    fastmath=True)
+def hist2D(
+        arr_a: np.ndarray, 
+        arr_b: np.ndarray, 
+        bin_a: int, 
+        bin_b: int) -> np.ndarray:
+    """calculates the cluster intersections between two arrays
+
+    e.g. ct[0][2]: 
+    the intersection of `arr_a` bin 0 & `arr_b` bin 2
+
+    inputs:
+        arr_a: np.ndarray
+            a 1D array where each value represents a the cluster
+            identity of a specific gene
+        arr_b: np.ndarray
+            a 1D array where each value represents a the cluster
+            identity of a specific gene
+        bin_a: np.ndarray
+            the number of clusters in array 1. Should be equivalent
+            to the maximum value in `arr_a` + 1
+        bin_b: np.ndarray
+            the number of clusters in array 1. Should be equivalent
+            to the maximum value in `arr_b` + 1
+
+    outputs:
+        ct: np.ndarray
+            a 2 dimensional array where each index represents
+            the intersection of the cluster identities in
+            `arr_a` and `arr_b`
+    """
+    assert arr_a.size == arr_b.size
+    ct = np.zeros((bin_a, bin_b), dtype=np.int32)
+    for x in np.arange(arr_a.size):
+        ct[arr_a[x]][arr_b[x]] += 1
+    return ct
+
+@nb.jit(
+    nopython=True, 
+    nogil=True, 
+    cache=True, 
+    fastmath=True)
+def hist3D(
+        arr_a: np.ndarray, 
+        arr_b: np.ndarray, 
+        arr_c: np.ndarray, 
+        bin_a: int, 
+        bin_b: int, 
+        bin_c: int) -> np.ndarray:
+    """calculates the bin intersections between three arrays
+
+    e.g. ct[0][2][1]: 
+    the intersection of `arr_a` bin 0 & `arr_b` bin 2 & `arr_c` bin 1
+
+    inputs:
+        arr_a: np.ndarray
+            a 1D array where each value represents a the cluster
+            identity of a specific gene
+        arr_b: np.ndarray
+            a 1d array where each value represents a the cluster
+            identity of a specific gene
+        arr_c: np.ndarray
+            a 1d array where each value represents a the cluster
+            identity of a specific gene
+        bin_a: np.ndarray
+            the number of clusters in array 1. Should be equivalent
+            to the maximum value in `arr_a` + 1
+        bin_b: np.ndarray
+            the number of clusters in array 1. Should be equivalent
+            to the maximum value in `arr_b` + 1
+        bin_c: np.ndarray
+            the number of clusters in array 1. Should be equivalent
+            to the maximum value in `arr_c` + 1
+
+    outputs:
+        ct: np.ndarray
+            a 3 dimensional array where each index represents
+            the intersection of the cluster identities in
+            `arr_a` and `arr_b` and `arr_c`
+    """
+    assert arr_a.size == arr_b.size
+    assert arr_b.size == arr_c.size
+    ct = np.zeros((bin_a, bin_b, bin_c), dtype=np.int32)
+    for x in np.arange(arr_a.size):
+        ct[arr_a[x]][arr_b[x]][arr_c[x]] += 1
+    return ct
 
 @nb.jit(nopython=True)
 def contingency_table(
@@ -21,13 +111,12 @@ def contingency_table(
 
 
 @nb.jit(nopython=True)
-def shuffle_bool_array(
-        bool_arr: np.ndarray) -> np.ndarray:
-    """Shuffles the genes for each observation independently
+def shuffle_bin_array(
+        bin_array: np.ndarray) -> np.ndarray:
+    """Shuffles the bin identities for a provided bin array
     """
-    shuf = bool_arr.copy()
-    for idx in range(bool_arr.shape[0]):
-        np.random.shuffle(shuf[idx])
+    shuf = bin_array.copy()
+    np.random.shuffle(shuf)
     return shuf
 
 
@@ -45,7 +134,7 @@ def empirical_pvalue(
 
 
 def hypergeometric_test(
-        e_bool: np.ndarray, 
+        e_bins: np.ndarray, 
         o_bool: np.ndarray) -> np.ndarray:
     """
     translating from: https://ars.els-cdn.com/content/image/1-s2.0-S1097276509008570-mmc1.pdf
@@ -55,9 +144,11 @@ def hypergeometric_test(
     n = N: number of genes in the bin
     N = M: total number of genes
     """
+    n_bins = e_bins.max() + 1
+    ct = hist2D(e_bins, o_bool, n_bins, 2)
     n = o_bool.sum()
-    k = (e_bool & o_bool).sum(axis=1)
-    N = e_bool.sum(axis=1)
+    k = ct[:, 1]
+    N = ct.sum(axis=1)
     M = o_bool.size
 
     # parameterize hypergeometric distribution
