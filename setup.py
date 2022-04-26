2d2356dbbf15aa805fd125eb01be2df6c4a64d68
Noam Teyssier <22600644+noamteyssier@users.noreply.github.com>
2022-04-26 15:36:03 -0700

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='pypage',
-   version='0.0.2',
+   version='0.0.3',
    description='Python implementation of the PAGE algorithm',
    author='Noam Teyssier',
    packages=['pypage'],
