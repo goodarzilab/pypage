a6d877d1523552f65e5398bd862766f1a4337892
noamteyssier <22600644+noamteyssier@users.noreply.github.com>
2022-05-04 15:30:16 -0700

    added tests for expression profile binning

--- tests/test_io.py
+++ tests/test_io.py
@@ -2,25 +2,45 @@
 """
 
 import pytest
+import numpy as np
 import pandas as pd
 from pypage import (
         ExpressionProfile,
         GeneOntology)
 
+N_GENES=1000
+N_BINS=5
+
 @pytest.fixture()
-def load_expression_dataframe():
-    return pd.read_csv(
-        "example_data/AP2S1.tab.gz", 
-        sep="\t",
-        header=None,
-        names=["gene", "bin"])
+def expression_dataframe():
+    genes = np.array([f"g.{g}" for g in np.arange(N_GENES)])
+    scores = np.random.normal(size=N_GENES)
+    return pd.DataFrame({
+        "gene": genes,
+        "score": scores})
 
+@pytest.fixture()
+def bins_dataframe():
+    genes = np.array([f"g.{g}" for g in np.arange(N_GENES)])
+    scores = np.random.choice(N_BINS, size=N_GENES)
+    return pd.DataFrame({
+        "gene": genes,
+        "score": scores})
 
-def test_load_expression(load_expression_dataframe):
-    frame = load_expression_dataframe
-    exp = ExpressionProfile(frame)
-    assert exp.n_genes == frame.gene.unique().size
-    assert exp.n_bins == frame.bin.unique().size
+def test_load_expression_nobins(expression_dataframe):
+    exp = ExpressionProfile(expression_dataframe)
+    assert exp.n_genes == N_GENES
+    assert exp.n_bins == 10
+
+def test_load_expression(expression_dataframe):
+    exp = ExpressionProfile(expression_dataframe, n_bins=N_BINS)
+    assert exp.n_genes == N_GENES
+    assert exp.n_bins == N_BINS
+
+def test_load_bins(bins_dataframe):
+    exp = ExpressionProfile(bins_dataframe, n_bins=N_BINS)
+    assert exp.n_genes == N_GENES
+    assert exp.n_bins == N_BINS
 
 
 def test_load_ontology():
