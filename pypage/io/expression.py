4c33ea050ba1df4dcb5ea802e084441d55df9232
noamteyssier <22600644+noamteyssier@users.noreply.github.com>
2022-05-04 16:45:11 -0700

    added splitting on equal size + option to switch between the two

--- pypage/io/expression.py
+++ pypage/io/expression.py
@@ -26,6 +26,8 @@ class ExpressionProfile:
         a (n_genes, ) array where each value represents the bin index of that gene
     bin_sizes: np.ndarray
         a (n_bins, ) array where each value represents the number of genes in that bin index
+    bin_ranges: Optional[np.ndarray]
+        a (n_bins, ) array describing the ranges of the scores for each bin. only present if `is_bin` is True 
 
     Methods
     -------
@@ -37,6 +39,7 @@ class ExpressionProfile:
             x: np.ndarray,
             y: np.ndarray,
             is_bin: bool = False,
+            bin_strategy: Optional[str] = None,
             n_bins: Optional[int] = None):
         """
         Parameters
@@ -48,10 +51,17 @@ class ExpressionProfile:
             of a specific gene, or the bin/cluster that gene belongs to.
         is_bin: bool
             whether the provided dataframe is prebinned. 
+        bin_strategy: str
+            the method to bin expression, choices are ['hist', 'split'].
+            'hist' will create a histogram with `n_bins` which will group
+            genes of similar counts more closely together but with unequal bin sizes. 
+            'split' will create bins of equivalent sizes. 
+            default = 'hist'
         n_bins: int
         """
         self._is_bin = is_bin
         self._validate_inputs(x, y)
+        self._set_bin_strategy(bin_strategy)
 
         self._load_genes(x)
         bins = self._load_expression(y, n_bins)
@@ -64,9 +74,25 @@ class ExpressionProfile:
             y: np.ndarray):
         """validates inputs are as expected
         """
-        assert x.size > 0
-        assert x.size == y.size
-        assert x.shape == y.shape
+        assert x.size > 0,\
+            "provided array must not be empty"
+        assert x.size == y.size,\
+            "genes and expression/bin arrays must be equal sized"
+        assert x.shape == y.shape,\
+            "genes and expression/bin arrays must be equally shaped"
+
+    def _set_bin_strategy(
+            self,
+            bin_strategy: str):
+        """validates and sets bin strategy
+        """
+        known_strategy = ["hist", "split"]
+        if not bin_strategy:
+            self._bin_strategy = "hist"
+        else:
+            assert bin_strategy in known_strategy,\
+                f"unknown bin strategy: `{bin_strategy}`. Known strategies : {', '.join(known_strategy)}"
+            self._bin_strategy = bin_strategy
 
     def _load_genes(
             self,
@@ -90,6 +116,8 @@ class ExpressionProfile:
             if not n_bins:
                 self.n_bins = 10
             else:
+                assert n_bins > 1,\
+                    "number of bins must be greater than 1"
                 self.n_bins = n_bins
             bins = self._build_bin_indices(expression)
             return self._load_bins(bins)
@@ -100,9 +128,51 @@ class ExpressionProfile:
             epsilon: float = 1e-6) -> np.ndarray:
         """converts expression data to binned data
         """
+        if self._bin_strategy == "hist":
+            return self._build_bin_hist(expression)
+        elif self._bin_strategy == "split":
+            return self._build_bin_split(expression)
+
+    def _build_bin_hist(
+            self,
+            expression: np.ndarray,
+            epsilon: float = 1e-6) -> np.ndarray:
+        """converts expression data to binned data using histogram method
+        """
         self.bin_sizes, self.bin_ranges = np.histogram(expression, bins=self.n_bins)
         self.bin_ranges[-1] += epsilon # added because digitize is not inclusive at maximum 
         return np.digitize(expression, self.bin_ranges)
+
+    def _build_bin_split(
+            self,
+            expression: np.ndarray) -> np.ndarray:
+        """converts expression data to binned data using equivlanet split method
+        """
+        argidx = np.argsort(expression)
+        max_size = expression.size
+        bin_size = int(max_size / self.n_bins)
+
+        bin_identities = np.zeros(max_size, dtype=int)
+        self.bin_sizes = np.zeros(self.n_bins, dtype=int)
+        self.bin_ranges = np.zeros(self.n_bins)
+        self.bin_ranges[-1] = expression.max()
+        
+        for i in np.arange(0, self.n_bins):
+            lower_bound = expression[argidx[bin_size * i]]
+
+            if i < self.n_bins - 1:
+                upper_bound = expression[argidx[bin_size * (i + 1)]]
+                mask = (expression >= lower_bound) & (expression < upper_bound)
+
+            # put remaining into last bin
+            else:
+                mask = (expression >= lower_bound)
+
+            self.bin_sizes[i] = mask.sum()
+            self.bin_ranges[i] = lower_bound
+            bin_identities[mask] = i
+
+        return bin_identities
         
     def _load_bins(
             self, 
