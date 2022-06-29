f192f510a92bce248e165902262e99c87b4f8a00
Noam Teyssier <22600644+noamteyssier@users.noreply.github.com>
2022-06-29 16:51:35 -0700

    consolidated test_run and test_run_2 to the same file

--- tests/test_run.py
+++ tests/test_run.py
@@ -8,6 +8,7 @@ from pypage import (
         ExpressionProfile, 
         GeneOntology)
 
+
 @pytest.fixture()
 def load_expression():
     frame = pd.read_csv(
@@ -21,6 +22,7 @@ def load_expression():
             frame.iloc[:, 1], 
             is_bin=True)
 
+
 @pytest.fixture()
 def load_ontology():
     frame = pd.read_csv(
@@ -32,6 +34,26 @@ def load_ontology():
     return GeneOntology(
             frame.iloc[:, 0],
             frame.iloc[:, 1])
+
+
+@pytest.fixture()
+def load_expression_bladder():
+    df = pd.read_csv('example_data/bladder_refseq.tsv.gz',
+                     sep="\t",
+                     header=0,
+                     names=["gene", "exp"])
+    exp = ExpressionProfile(df.iloc[:, 0],
+                             df.iloc[:, 1],
+                             bin_strategy='split',
+                            n_bins=20)
+    exp.convert_from_to('refseq', 'ensg', 'human')
+    return exp
+
+
+@pytest.fixture()
+def load_ontology_cistrome():
+    ont = GeneOntology(ann_file='example_data/hg38_cistrome_index_truncated.txt.gz')
+    return ont
 
 
 def test_init(load_expression, load_ontology):
@@ -115,4 +137,25 @@ def test_empty_summary(load_expression, load_ontology):
     except ValueError:
         assert True
 
+
+def test_heatmap_conversion(load_expression_bladder, load_ontology_cistrome):
+    p = PAGE(
+        load_expression_bladder,
+        load_ontology_cistrome,
+        n_shuffle=10,
+        k=5)
+    results = p.run()
+    hm = p.heatmap()
+    hm.convert_from_to('gs', 'ensg', 'human')
+
+
+def test_heatmap_save(load_expression_bladder, load_ontology_cistrome):
+    p = PAGE(
+        load_expression_bladder,
+        load_ontology_cistrome,
+        n_shuffle=10,
+        k=5)
+    results = p.run()
+    hm = p.heatmap()
+    hm.save('test_heatmap', show_reg=True)
 
