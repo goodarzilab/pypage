71c73269c0d5096ef2fc281f4043d53e044133b1
Artemy Bakulin <logic2000.bakulin@gmail.com>
2023-06-18 22:25:19 +0300

    manual link fixed

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
    name='bio-pypage',
-   version='0.1.4',
+   version='0.1.5',
    description='Python implementation of the PAGE algorithm',
    author='Artemy Bakulin, Noam Teyssier',
    long_description=long_description,
